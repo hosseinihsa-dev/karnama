170ba239865d14690ad0dc9707e642f43ebaa6b6
hosseinihsa-dev <hosseini.hsa@gmail.com>
2026-09-09 00:46:38 +0330

    نسخه ۲۱

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C = 'karnama-v20';
-const FILES = ['./', './index.html', './app.js?v=20', './style.css?v=20', './manifest.json', './icon-192.png', './icon-512.png'];
+const C = 'karnama-v21';
+const FILES = ['./', './index.html', './app.js?v=21', './style.css?v=21', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
 });
