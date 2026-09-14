d60246f157bd887a27db3c5377b7297f0c906c41
hosseinihsa-dev <hosseini.hsa@gmail.com>
2026-09-14 16:50:24 +0330

    نسخه ۳۵: شماره نسخه و کش

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C = 'karnama-v34';
-const FILES = ['./', './index.html', './core.js?v=34', './study.js?v=34', './views.js?v=34', './style.css?v=34', './manifest.json?v=34', './icon-192.png?v=34', './icon-512.png?v=34', './icon-maskable.png?v=34', './logo-karnama.png?v=34'];
+const C = 'karnama-v35';
+const FILES = ['./', './index.html', './core.js?v=35', './study.js?v=35', './views.js?v=35', './style.css?v=35', './manifest.json?v=35', './icon-192.png?v=35', './icon-512.png?v=35', './icon-maskable.png?v=35', './logo-karnama.png?v=35'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => Promise.all(FILES.map(f => c.add(f).catch(() => {})))).then(() => self.skipWaiting()));
 });
